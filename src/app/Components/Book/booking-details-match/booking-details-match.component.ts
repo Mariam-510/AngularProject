a0e351c25e593442cc5ed61aa69e75c2909b6b2f
Mariam-510 <mariamashrafamin@gmail.com>
2025-03-28 19:40:39 +0200

    add folders

---
+++ src/app/Components/Book/booking-details-match/booking-details-match.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-details-match',
+  imports: [],
+  templateUrl: './booking-details-match.component.html',
+  styleUrl: './booking-details-match.component.css'
+})
+export class BookingDetailsMatchComponent {
+
+}
---
+++ src/app/Components/Book/booking-match/booking-match.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-match',
+  imports: [],
+  templateUrl: './booking-match.component.html',
+  styleUrl: './booking-match.component.css'
+})
+export class BookingMatchComponent {
+
+}
