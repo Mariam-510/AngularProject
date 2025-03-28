d7c4ca9713137252ba948d85456be716593cb7ea
Mariam-510 <mariamashrafamin@gmail.com>
2025-03-28 16:09:55 +0200

    update entertainment components

--- src/app/Components/Entertainment/eslider/eslider.component.ts
+++ src/app/Components/Entertainment/eslider/eslider.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+declare var bootstrap: any; // Required for Bootstrap modal handling
+
 @Component({
   selector: 'app-eslider',
   imports: [CommonModule, RouterModule],
@@ -18,7 +20,13 @@ export class ESliderComponent {
       title: '& JULIET',
       date: 'August 13 - September 7',
       rating: 4.5,
-      description: "It's not going to win Nobel prizes or Pulitzers, but it will keep you entertained for two and a half hours."
+      description: "It's not going to win Nobel prizes or Pulitzers, but it will keep you entertained for two and a half hours.",
+      isFavorite: false,
+      word: "Trending",
+      reviews: 6,
+      price: 40,
+      qoute: "Your Next Adventure Awaits!",
+      subQoute: "Book now and be part of something extraordinary."
     },
     {
       image: 'img/2.jpg',
@@ -27,18 +35,48 @@ export class ESliderComponent {
       title: '& JULIET',
       date: 'August 13 - September 7',
       rating: 3.5,
-      description: "It's not going to win Nobel prizes or Pulitzers, but it will keep you entertained for two and a half hours."
+      description: "It's not going to win Nobel prizes or Pulitzers, but it will keep you entertained for two and a half hours.",
+      isFavorite: false,
+      word: "Must-See",
+      reviews: 6,
+      price: 50,
+      qoute: "Unmissable Experience!",
+      subQoute: "Join us for an unforgettable journey."
     },
     {
       image: 'img/3.jpg',
       location: 'Ahmanson Theater',
-      category: 'Theater',
+      category: 'Concerts',
       title: '& JULIET',
       date: 'August 13 - September 7',
       rating: 4,
-      description: "It's not going to win Nobel prizes or Pulitzers."
+      description: "It's not going to win Nobel prizes or Pulitzers, but it will keep you entertained for two and a half hours.",
+      isFavorite: false,
+      word: "Top Pick",
+      reviews: 6,
+      price: 35,
+      qoute: "Experience the Magic Live!",
+      subQoute: "Don't miss out—secure your tickets today!"
     }
   ];
 
+
   Math = Math;
+
+  toggleFavorite(event: any) {
+    event.isFavorite = !event.isFavorite;
+  }
+
+
+  // ----------------------------------------------------------------------------------
+
+  categoryIcons: { [key: string]: string } = {
+    'Concerts': '🎵',          // Music Note
+    'Theater': '🎭',           // Theater Masks
+    'Dance': '💃',             // Dancing Emoji
+    'Stand-Up Comedy': '🎤',   // Microphone
+    'Other': '✨',             // Sparkles (for other)
+  };
+
+
 }
